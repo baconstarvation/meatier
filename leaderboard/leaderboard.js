59861a861914486fa26cd7962c6d37dc302dfaee
baconstarvation <ant.ucd@gmail.com>
2015-04-11 13:16:51 -0700

    on 6.2 Event Selectors

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,9 +2,18 @@ PlayersList = new Mongo.Collection('players');
 
 
 if(Meteor.isClient) {
+	
 	Template.leaderboard.helpers({
 		'player': function(){
 			return PlayersList.find()
 		}
 	});
+
+		Template.leaderboard.events({
+		'click': function(){
+			console.log("You clicked something");
+		}
+	});
 };
+
+
